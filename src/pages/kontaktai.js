05d4ecdd81827c4676fd628b83b71d92cc4feb8b
Paulius Gorodeckis <kaffau@gmail.com>
2020-06-19 15:47:27 +0300

    Adjusted style

--- src/pages/kontaktai.js
+++ src/pages/kontaktai.js
@@ -11,7 +11,7 @@ const ContactPage = ({
   return (
     <Layout>
       <Helmet>
-        <title>Contact — {site.siteMetadata.title}</title>
+        <title>Kontaktai — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <div className="two-grids -contact">
@@ -22,19 +22,19 @@ const ContactPage = ({
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
             <div>
-              <label htmlFor="w3lName">Name</label>
+              <label htmlFor="w3lName">Vardas</label>
               <input type="text" name="w3lName" id="w3lName"/>
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
+              <label htmlFor="w3lSender">El. paštas</label>
               <input type="email" name="w3lSender" id="w3lSender"/>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
+              <label htmlFor="w3lSubject">Tema</label>
               <input type="text" name="w3lSubject" id="w3lSubject"/>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
+              <label htmlFor="w3lMessage">Žinutė</label>
               <textarea name="w3lMessage" id="w3lMessage"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
