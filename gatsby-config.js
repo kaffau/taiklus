ed24061480fadc100396e190ba761fc7ac84dbe8
Paulius Gorodeckis <kaffau@gmail.com>
2020-07-03 21:41:15 +0300

    remove sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,6 @@ module.exports = {
     'gatsby-plugin-dark-mode',
     // siteURL is a must for sitemap generation
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
   ],
 }
